a9ba5fe112e9fb03123edf4afc9b619bfba8a6d7
Au673664 <Au673664@uni.au.dk>
2025-03-03 09:55:51 +0100

    Spaces on line 3

--- src/app/services/anthropic-service.service.ts
+++ src/app/services/anthropic-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import Anthropic from '@anthropic-ai/sdk';
-import {environment} from "../../environments/environment";
+import { environment } from "../../environments/environment";
 
 
 @Injectable({
